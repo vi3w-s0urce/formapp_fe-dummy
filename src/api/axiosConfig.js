41473f7d1c0cc358b6372cbc7c148d7904720999
xG4L1L30x <macdizofti032@gmail.com>
2024-11-04 16:54:54 +0700

    update: change API url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import { useAuth } from "../context/AuthContext";
 
 export const axiosBase = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'https://api.formapp.viewsource.work/api/',
 });
 
 export const axiosAuth = (token) => axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'https://api.formapp.viewsource.work/api/',
     headers: {
         'Authorization' : token,
         'Content-type': 'application/json'
